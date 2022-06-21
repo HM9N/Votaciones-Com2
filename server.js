99b574f22046d7b992d4b889c8d45c493251351f
hm9n <jhonmh9n@gmail.com>
2022-06-20 22:42:11 -0500

    working

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+
+const port = 9000;
+
+app.use(express.static("client"));
+
+// Se inicia el servidor y se inicializa socket.io 
+const server = app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+const io = require("socket.io")(server);
+const clients = [];
+
+// Se inicializan las opciones
+const options = {
+    "0": { votes: 0, label: "JavaScript", color: randomRGB() },
+    "1": { votes: 0, label: "C#", color: randomRGB() },
+    "2": { votes: 0, label: "PHP", color: randomRGB() },
+    "3": { votes: 0, label: "Python", color: randomRGB() },
+    "4": { votes: 0, label: "Go", color: randomRGB() }
+};
+
+// Conexión con el cliente
+io.on("connection", (socket) => {
+    io.emit("update", options);
+
+    //Cuando se hace el voto
+    socket.on("vote", (index) => {
+        if (clients.includes(socket.id)) {
+            console.log("ya está");
+        } else {
+
+            clients.push(socket.id);
+            console.log(clients);
+
+            if (options[index]) {
+                options[index].votes += 1;
+            }
+
+            //Se hace update
+            io.emit("update", options);
+
+        }
+    });
+});
+
+// Para generar los colores aleatoriamente
+function randomRGB() {
+    const r = () => Math.random() * 256 >> 0;
+    return `rgb(${r()}, ${r()}, ${r()})`;
+}
